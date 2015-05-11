af689a1f134e58cf92426a01ea730a3a9059d657
bjzhou <bjzhou1990@gmail.com>
2015-05-11 22:37:52 +0800

    add password crypt

--- controllers/user.js
+++ controllers/user.js
@@ -3,9 +3,11 @@
  */
 
 var User = require('../models/user');
+var bcrypt = require('bcrypt');
 
 exports.register = function (req, res, next) {
     var _user = req.body.user;
+    //console.log(_user);
     User.findOne({username: _user.username}, function (err, mres) {
         if (err) return next(err);
         if (mres) {
@@ -15,22 +17,24 @@ exports.register = function (req, res, next) {
             });
         } else {
             if (_user.password === _user.repeatPassword) {
-                var user = new User({username: _user.username, password: _user.password});
-                user.save(function (err) {
-                    if (err) return next(err);
-                    res.json({
-                        status: "200",
-                        msg:    "注册成功"
-                    })
-                })
+                cryptPassword(_user.password, function (err, hash) {
+                    var user = new User({username: _user.username, password: hash});
+                    user.save(function (err) {
+                        if (err) return next(err);
+                        res.json({
+                            status: "200",
+                            msg:    "注册成功"
+                        });
+                    });
+                });
             } else {
                 res.json({
                     status: "10002",
                     msg:    "两次输入的密码不一致"
-                })
+                });
             }
         }
-    })
+    });
 };
 
 exports.login = function (req, res, next) {
@@ -38,24 +42,46 @@ exports.login = function (req, res, next) {
     User.findOne({username: _user.username}, function (err, mres) {
         if (err) return next(err);
         if (mres) {
-            console.log(mres)
-            if (_user.password === mres.password) {
-                res.json({
-                    uid: mres.uid,
-                    username: mres.username,
-                    created: mres.created
-                });
-            } else {
-                res.json({
-                    status: "10003",
-                    msg:    "密码错误"
-                });
-            }
+            //console.log(mres)
+            comparePassword(_user.password, mres.password, function (err, isMatch) {
+                if (isMatch) {
+                    res.json({
+                        uid: mres.uid,
+                        username: mres.username,
+                        created: mres.created
+                    });
+                } else {
+                    res.json({
+                        status: "10003",
+                        msg:    "密码错误"
+                    });
+                }
+            });
         } else {
             res.json({
                 status: "10004",
                 msg:    "账号不存在"
             });
         }
-    })
+    });
+};
+
+function cryptPassword(password, callback) {
+   bcrypt.genSalt(10, function(err, salt) {
+    if (err) 
+      return callback(err);
+
+    bcrypt.hash(password, salt, function(err, hash) {
+      return callback(err, hash);
+    });
+
+  });
+};
+
+function comparePassword(userPassword, password, callback) {
+   bcrypt.compare(userPassword, password, function(err, isPasswordMatch) {
+      if (err) 
+        return callback(err);
+      return callback(null, isPasswordMatch);
+   });
 };
